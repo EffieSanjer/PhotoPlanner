dabecbbb38e2dd5a537806f7a5f645bedbac3a55
Светлана Ермолаева <sv.ermolaeva@yandex.ru>
2022-02-22 19:20:00 +0800

    2.0

---
+++ PhotoSS/PhotoSS/Ph_calendar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Plugin.Calendar;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace PhotoSS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ph_calendar : ContentPage
+    {
+        public Ph_calendar()
+        {
+            InitializeComponent();
+           
+
+
+        }
+       
+}
+}
